4e50d707048e5f9d962a299cc946e003f28c204b
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-04-16 00:59:24 +0530

    parent (main)/layout route protection

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,14 +1,6 @@
-import { getUserOnboardingStatus } from '@/actions/user';
-import { redirect } from 'next/navigation';
 import React from 'react'
 
 const IndustryInsightsPage = async () => {
-    //redirect to onboarding if not onboarded
-    const { isOnboarded } = await getUserOnboardingStatus();
-    if(!isOnboarded) {
-        console.log("User is not onboarded")
-        redirect('/onboarding');
-    }
     
     return (
         <div>
