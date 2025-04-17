172d7558d222df3c937abc10c35df5205829673d
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-04-17 23:52:07 +0530

    onboarding form zod schema

--- app/(main)/onboarding/_components/onboarding-form.tsx
+++ app/(main)/onboarding/_components/onboarding-form.tsx
@@ -1,4 +1,11 @@
-import React from 'react'
+'use client';
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { onboardingSchema } from "@/app/lib/schema";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 interface OnboardingFormProps {
     industries: {
@@ -9,11 +16,34 @@ interface OnboardingFormProps {
 }
 
 const OnboardingForm = ({ industries } : OnboardingFormProps) => {
+    const [selectedIndustry, setSelectedIndustry] = useState<string | null>(null);
+    const router = useRouter();
+
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors
+        },
+        setValue,
+        watch
+    } = useForm({
+        resolver: zodResolver(onboardingSchema),
+    });
+
     return (
-        <div>
-            ONBOARDING FORM
+        <div className="flex items-center justify-center">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Onboarding Form</CardTitle>
+                    <CardDescription></CardDescription>
+                </CardHeader>
+                <CardContent>
+                    
+                </CardContent>
+            </Card>
         </div>
     )
 }
 
-export default OnboardingForm
+export default OnboardingForm;
