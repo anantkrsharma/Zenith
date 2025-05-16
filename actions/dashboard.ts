8835cc9e23686e6e8deea548d6a2b13f45c0ff69
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-05-17 03:31:24 +0530

    added GeminiAPI + implemented ai creation of industry insight

--- actions/dashboard.ts
+++ actions/dashboard.ts
@@ -2,7 +2,44 @@
 
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import {GoogleGenAI} from '@google/genai';
 
+//Gemini AI instance
+const ai = new GoogleGenAI({
+    apiKey: process.env.GENAI_API_KEY
+});
+
+const generateAiInsight = async (industry: string | null) => {
+    const prompt = `
+        Analyze the current state of the ${industry} industry and provide insights in ONLY the following JSON format without any additional notes or explanations:
+        {
+            "salaryRanges": [
+            { "role": "string", "min": number, "max": number, "median": number, "location": "string" }
+            ],
+            "growthRate": number,
+            "demandLevel": "High" | "Medium" | "Low",
+            "topSkills": ["skill1", "skill2"],
+            "marketOutlook": "Positive" | "Neutral" | "Negative",
+            "keyTrends": ["trend1", "trend2"],
+            "recommendedSkills": ["skill1", "skill2"]
+        }
+        
+        IMPORTANT: Return ONLY the JSON. No additional text, notes, or markdown formatting.
+        Include at least 5 common roles for salary ranges.
+        Growth rate should be a percentage.
+        Include at least 5 skills and trends.
+    `;
+    
+    const result = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: prompt
+    });
+
+    let text = result.text ?? "";
+    text = text.replace(/```(?:json)?\n?/g, "").trim();
+
+    return JSON.parse(text);
+}
 
 export async function getIndustryInsights(){
     try {
@@ -14,15 +51,37 @@ export async function getIndustryInsights(){
         const user = await db.user.findUnique({
             where: {
                 clerkUserId: userId
-            }  
+            },
+            include: {
+                industryInsight: true
+            }
         });
         if(!user) {
             throw new Error("User not found");
         }
 
-        
+        if(!user.industryInsight) {
+            const insights = await generateAiInsight(user.industry);
 
+            const industryInsight = await db.industryInsight.create({
+                data: {
+                    industry: user.industry,
+                    ...insights,
+                    nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), //7 days from now
+                }
+            })
+
+            return industryInsight;
+        }
+
+        return user.industryInsight;
     } catch (error) {
-        
+        if (error instanceof Error) {
+            console.log("Error while getting the industry insights:", error.message);
+            throw new Error("Error while getting the industry insights. Please try again later.");
+        } else {
+            console.log("An unknown error occurred while getting the industry insights.");
+            throw new Error("An unknown error occurred while getting the industry insights. Please try again later.");
+        }
     }
 }
