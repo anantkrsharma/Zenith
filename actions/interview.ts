da9010b7af6d6bf7eec28a3fc4f725b6dc3f07e2
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-06-12 01:26:35 +0530

    added ai generated interview assessment logic

--- actions/interview.ts
+++ actions/interview.ts
@@ -8,7 +8,8 @@ const ai = new GoogleGenAI({
     apiKey: process.env.GENAI_API_KEY
 });
 
-export async function generateInterviewQuiz(industry: string) {
+//fn to generate interview quiz questions based on user's industry and skills
+export async function generateInterviewQuestions(industry: string) {
     try {
         const { userId } = await auth();
         if (!userId) {
@@ -49,9 +50,9 @@ export async function generateInterviewQuiz(industry: string) {
         let text = result.text ?? "";
         text = text.replace(/```(?:json)?\n?/g, "").trim();
 
-        const interviewQuiz  = JSON.parse(text);
+        const interviewQuestions = JSON.parse(text);
         
-        return interviewQuiz.questions;
+        return interviewQuestions.questions;
     } catch (error) {
         if (error instanceof Error) {
             console.error("Error generating interview questions:", error.message);
@@ -59,6 +60,84 @@ export async function generateInterviewQuiz(industry: string) {
         } else {
             console.error("An unknown error occurred while generating interview questions.");
             throw new Error("An unknown error occurred while generating interview questions.");
+        }
+    }
+}
+
+interface QuestionDataProps {
+    question: string;
+    correctAnswer: string;
+    explanation: string;
+}
+
+//fn to save interview quiz assessment based on user's answers
+export async function saveInterviewAssessment(questions: QuestionDataProps[], answers: string[], score: number) {
+    try {
+        const { userId } = await auth();
+        if (!userId) {
+            throw new Error("Unauthenticated");
+        }
+
+        const user = await db.user.findUnique({
+            where: { clerkUserId: userId },
+        });
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        const questionData = questions.map((q, index) => ({
+            question: q.question,
+            correctAnswer: q.correctAnswer,
+            userAnswer: answers[index],
+            isCorrect: q.correctAnswer === answers[index],
+            explanation: q.explanation,
+        }));
+
+        const wrongAnswers = questionData.filter((quesData) => quesData.isCorrect === false);
+
+        let improvementTip = "";
+
+        if(wrongAnswers.length > 0){
+            const wrongAnswerDetails = wrongAnswers.map((wrongAns) => `
+                Question: ${wrongAns.question}\nCorrect Answer: ${wrongAns.correctAnswer}\nUser's Answer: ${wrongAns.userAnswer}
+            `).join("\n\n");
+
+            const improvementTipPrompt = `
+                The user got the following ${user.industry} technical interview questions wrong:
+
+                ${wrongAnswerDetails}
+
+                Based on these mistakes, provide a concise, specific improvement tip.
+                Focus on the knowledge gaps revealed by these wrong answers.
+                Keep the response under 2 sentences and make it encouraging.
+                Don't explicitly mention the mistakes, instead focus on what to learn/practice.
+            `;
+            
+            const res = await ai.models.generateContent({
+                model: "gemini-2.0-flash",
+                contents: improvementTipPrompt
+            });
+            improvementTip = res.text?.trim() || "";
+        }
+        
+        const interviewAssessment = await db.assessment.create({
+            data: {
+                userId: user.id,
+                quizScore: score,
+                questions: questionData,
+                improvementTip,
+                category: "Technical",
+            }
+        });
+
+        return interviewAssessment;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error("Error saving interview quiz:", error.message);
+            throw new Error("Failed to save interview quiz");
+        } else {
+            console.error("An unknown error occurred while saving interview quiz.");
+            throw new Error("An unknown error occurred while saving interview quiz.");
         }
     }
 }
