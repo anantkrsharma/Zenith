d1b8b426e7f46c32b6fc7c7aa8e1625e21abe252
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-06-21 22:59:48 +0530

    quiz-list component

--- app/(main)/interview/_components/quiz-list.tsx
+++ app/(main)/interview/_components/quiz-list.tsx
@@ -1,16 +1,28 @@
+'use client';
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Assessment } from '@/lib/generated/client'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 
 const QuizList = ({ assessments }: { assessments: Assessment[] }) => {
+    const router = useRouter();
+    const [selectedQuiz, setSelectedQuiz] = useState(null);
+
     return (
         <div>
-            {
-                assessments.map((ass, index) => (
-                    <div key={index}>
-                        {JSON.stringify(ass)}
-                    </div>
-                ))
-            }
+            <Card>
+                <CardHeader>
+                    <CardTitle>Card Title</CardTitle>
+                    <CardDescription>Card Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
+            </Card>
         </div>
     )
 }
