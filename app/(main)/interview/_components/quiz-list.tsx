a8ffda871ba42ec6f94b6ebfa562a0fd6609f94e
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-06-21 23:52:45 +0530

    quiz-list component

--- app/(main)/interview/_components/quiz-list.tsx
+++ app/(main)/interview/_components/quiz-list.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Assessment } from '@/lib/generated/client'
 import { useRouter } from 'next/navigation'
@@ -10,11 +11,25 @@ const QuizList = ({ assessments }: { assessments: Assessment[] }) => {
     const [selectedQuiz, setSelectedQuiz] = useState(null);
 
     return (
-        <div>
+        <>
             <Card>
                 <CardHeader>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardDescription>Card Description</CardDescription>
+                    <div>
+                        <CardTitle className='gradient-title text-3xl md:text-4xl'>
+                            Recent Quizzes
+                        </CardTitle>
+                        <CardDescription className='text-muted-foreground'>
+                            Review you past quiz performance
+                        </CardDescription>
+                    </div>
+                    <div className='flex justify-center py-4'>
+                        <Button 
+                            variant={'outline'}
+                            className='w-fit sm:px-auto sm:w-[50%] md:w-[55%] py-5 flex items-center justify-center gap-1 bg-zinc-900 border-neutral-700 hover:cursor-pointer hover:bg-neutral-800 hover:border-zinc-500 transition-all duration-75 ease-in-out'
+                            >
+                            Start New Quiz
+                        </Button>
+                    </div>
                 </CardHeader>
                 <CardContent>
                     <p>Card Content</p>
@@ -23,7 +38,7 @@ const QuizList = ({ assessments }: { assessments: Assessment[] }) => {
                     <p>Card Footer</p>
                 </CardFooter>
             </Card>
-        </div>
+        </>
     )
 }
 
