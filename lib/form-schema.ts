c6997384a473dd8c55d319f609007efc5b18fa7b
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-06-30 23:15:13 +0530

    feat: resume form schema

--- lib/form-schema.ts
+++ lib/form-schema.ts
@@ -1,5 +1,7 @@
+import { Description } from '@radix-ui/react-dialog';
 import { z } from 'zod';
 
+//onboarding page form schema
 export const onboardingSchema = z.object({
     industry: z.string({
         required_error: "Please select an industry"
@@ -22,6 +24,85 @@ export const onboardingSchema = z.object({
             .transform((val) => val ? val.split(",").map((skill) => skill.trim()).filter(Boolean) : undefined)
 })
 
+//resume form schema
+export const contactSchema = z.object({
+    email: z.string().email("Invalid email address"),
+    mobile: z.number().optional(),
+    linkedin: z.string().optional(),
+    twitter: z.string().optional()
+});
+
+const educationSchema = z.object({
+        title: z.string().min(1, "Education details are required"),
+        institute: z.string().min(1, "Institute is required"),
+        description: z.string().optional(),
+        startDate: z.string().min(1, "Start date is required"),
+        endDate: z.string().optional(),
+        current: z.boolean().default(false)
+    })
+    .refine((data) => {
+        if(!data.current && !data.endDate)
+            return false;
+        
+        return true;
+    }, {
+        message: "End date is required unless you're not studying here currently",
+        path: ["endDate"]
+    });
+
+const workExpSchema = z.object({
+        title: z.string().min(1, "Title is required"),
+        company: z.string().min(1, "Organization is required"),
+        description: z.string().min(1, "Description is required"),
+        startDate: z.string().min(1, "Start date is required"),
+        endDate: z.string().optional(),
+        current: z.boolean().default(false),
+    })
+    .refine((data) => {
+        if(!data.current && !data.endDate)
+            return false;
+        
+        return true;
+    }, {
+        message: "End date is required unless this is your current position",
+        path: ["endDate"]
+    })
+
+const githubUrlRegex = /^https?:\/\/(www\.)?github\.com\/[A-Za-z0-9_.-]+(\/[A-Za-z0-9_.-]+)?(\/)?$/;
+
+const projectSchema = z.object({
+        title: z.string().min(1, "Title is required"),
+        description: z.string().min(1, "Description is required"),
+        github: z
+                .string()
+                .url()
+                .refine(
+                    (url) => githubUrlRegex.test(url),
+                    { message: "Invalid GitHub URL" }
+                )
+                .optional(),
+        lievLink:   z
+                    .string()
+                    .url(),
+        startDate: z.string().min(1, "Start date is required"),
+        endDate: z.string().optional(),
+        current: z.boolean().default(false)
+    })
+    .refine((data) => {
+        if(!data.current && !data.endDate)
+            return false;
+        
+        return true;
+    }, {
+        message: "End date is required unless working on the project currently",
+        path: ["endDate"]
+    });
+
 export const resumeSchema = z.object({
-    
+    contactInfo: contactSchema,
+    summary: z.string().min(1, "Enter more details in your summary").optional(),
+    skills: z.string().min(1, "Skills are required"),
+    education: z.array(educationSchema),
+    workExp: z.array(workExpSchema),
+    projects: z.array(projectSchema)
 })
