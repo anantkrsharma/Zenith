6e43f40013eff21646a36a3280e4edd6363b645b
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-07-27 20:50:39 +0530

    fix: build fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,8 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-expressions": "off",
       "@typescript-eslint/no-unnecessary-type-constraint": "off",
       "@typescript-eslint/no-wrapper-object-types": "off",
-      "@typescript-eslint/no-this-alias": "off"
+      "@typescript-eslint/no-this-alias": "off",
+      '@typescript-eslint/no-empty-object-type': 'off'
     },
   },
 ];
