b4697c220c8d896a5988d5963728d206ae245c5f
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-07-27 18:38:31 +0530

    chores

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,11 +2,9 @@ import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { updateIndustryInsights } from "@/lib/inngest/functions";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
-        // Inngest functions
         updateIndustryInsights
     ],
 });
