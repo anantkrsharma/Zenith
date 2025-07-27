0f76dd381ce8f781e2f37e30f9fccd7336afe6ad
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-07-28 01:12:59 +0530

    style: ui/ux

--- app/(main)/interview/_components/quiz.tsx
+++ app/(main)/interview/_components/quiz.tsx
@@ -262,7 +262,7 @@ export const Quiz = () => {
                                     </Button>
                                     : 
                                     <Button variant={'outline'} 
-                                            className='flex items-center justify-center gap-1 bg-zinc-900 border-blue-900 hover:cursor-pointer hover:bg-neutral-800 hover:border-blue-700 transition-all duration-75 ease-in-out'
+                                            className='flex items-center justify-center gap-1 bg-zinc-900 border-cyan-900 hover:cursor-pointer hover:bg-neutral-800 hover:border-cyan-800 transition-all duration-75 ease-in-out'
                                             onClick={() => {
                                                 setShowExplanation(true)
                                                 setDisableQuizOptn(true)
